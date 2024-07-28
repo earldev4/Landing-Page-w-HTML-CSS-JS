ce1343c43839ea5e1ccec4f0d037eed95ba9a03d
Deva Ahmad <devaahmad43@gmail.com>
2024-07-28 22:00:56 +0700

    menambahkan filter ke header

--- landingpage.js
+++ landingpage.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     
     let lightbutton = $('.light')
+    let changebanner = $('.intro')
     let allcontainer = $('.allcontainer')
     let alltexts = $('.alltexts')
     let changelight = function(){
         lightbutton.toggleClass('dark')
+        changebanner.toggleClass('givefilter')
         allcontainer.toggleClass('changebg')
         alltexts.toggleClass('changefont')
     }
